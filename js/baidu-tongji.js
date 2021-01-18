4e7e39cccb362e49dc472a7b3cbe90da912c7f66
YUE <peng.yue@gpower.co>
2021-01-18 17:58:38 +0800

    Deployed 7709952 with MkDocs version: 1.1.2

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function () {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?51fafb2ac8f80ecf43b05bb4cc281020";
+    hm.src = "https://hm.baidu.com/hm.js?642bfc37120fa0c601de3ecd298417b0";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
